8624cc0c2bdc5f734809932912af697f0308ea8a
Ali TANIRLAR <alitanirlar@gmail.com>
2023-09-21 07:58:02 +0300

    Update app.

--- config/initializers/apple-app-site-association.rb
+++ config/initializers/apple-app-site-association.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 Apple::App::Site::Association.configure do |c|
-  c.details({ appID: '9JA89QQLNQ.com.apple.wwdc', paths: ['/wwdc/news/', '/videos/wwdc/2015/*'] },
-             { appID: 'ABCD1234.com.apple.wwdc', paths: '*' })
+  c.details({ appID: 'SZ774FYKCJ.com.thingtells.grungard-blt', paths: ['addDeviceWithLink/*'] })
 end
